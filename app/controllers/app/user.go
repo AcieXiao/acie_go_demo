05bfa9ac6e53d7cd26234967aade4316908451bc
AcieXiao <38552190+AcieXiao@users.noreply.github.com>
2023-04-24 06:39:26 +0000

    up

---
+++ app/controllers/app/user.go
@@ -0,0 +1,23 @@
+package app
+
+import (
+	"acie-gin/app/common/request"
+	"acie-gin/app/common/response"
+	"acie-gin/app/services"
+	"github.com/gin-gonic/gin"
+)
+
+// Register 用户注册
+func Register(c *gin.Context) {
+	var form request.Register
+	if err := c.ShouldBindJSON(&form); err != nil {
+		response.ValidateFail(c, request.GetErrorMsg(form, err))
+		return
+	}
+
+	if err, user := services.UserService.Register(form); err != nil {
+		response.BusinessFail(c, err.Error())
+	} else {
+		response.Success(c, user)
+	}
+}
